f477911627f67730dc0af67bc921947a1a373bc4
kakiufong <fongkakiu2001@gmail.com>
2023-03-19 19:00:46 +0800

    pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,17 @@
 module.exports = {
   apps: [{
-    script: 'npm start'
+    name: 'ierg4210_assignment',
+    script: 'npm start',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
   }],
 
   deploy: {
